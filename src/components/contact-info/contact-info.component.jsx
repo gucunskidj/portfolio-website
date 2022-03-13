b3e5293bdbd790228812962cac3163f34f8dc457
Đorđe Gucunski <64844595+gucunskidj@users.noreply.github.com>
2022-03-13 09:57:05 +0100

    Update contact-info.component.jsx

--- src/components/contact-info/contact-info.component.jsx
+++ src/components/contact-info/contact-info.component.jsx
@@ -14,7 +14,7 @@ const ContactInfo = () => {
         ))
       }
       <span>Download my resume:</span>
-      <button><a href="/dordegucunski.pdf" download>Resume</a></button>
+      <button><a href="/djordjegucunskiresume.pdf" download>Resume</a></button>
     </div>
   )
 }
